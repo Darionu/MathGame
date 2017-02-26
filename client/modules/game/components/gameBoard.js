79a1e298c28b5c0f8105daca16728082464b80d3
Tomasz Przytuła <t.przytula@cctechnology.pl>
2017-02-26 23:04:01 +0100

    add alpha version information during game

--- client/modules/game/components/gameBoard.js
+++ client/modules/game/components/gameBoard.js
@@ -6,6 +6,7 @@ const GameBoard = class extends React.Component {
     render() {
         return (
             <div className={styles.gameBoard}>
+                ALPHA VERSION! TO END GAME SIMPLY LOG OUT! <br/>
                 EQUATION <br/>
                 {this.props.firstNumber} {this.props.equation} {this.props.secondNumber} = ?
 
